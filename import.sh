02708687e6951105d75d86b16b0e48bcf5e8bac7
Rolf Schröder <rolf.schr@gmail.com>
2020-09-23 12:41:05 +0200

    First impl of import.sh & github worktflow

---
+++ import.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export TEMPDIR=$(mktemp -d -p "$DIR")
+export DATADIR="${TEMPDIR}/pgdata"
+export RUNDIR="${TEMPDIR}/run"
+
+cleanup() {
+  pg_ctl -D "$DATADIR" stop
+  rm -rf "$TEMPDIR"
+}
+
+trap cleanup EXIT
+
+ENCODING="UTF-8"
+mkdir -p "$RUNDIR"
+initdb -D "$DATADIR" -E $ENCODING
+pg_ctl -D "$DATADIR" -o "-k $RUNDIR" -l "$DATADIR/logfile" start
+createuser --host "$RUNDIR" --no-createrole --no-superuser --login --inherit --createdb vulnerablecode
+createdb   --host "$RUNDIR" -E $ENCODING --owner=vulnerablecode --user=vulnerablecode --port=5432 vulnerablecode
+(
+  export DJANGO_DEV=1
+  ${out}/manage.py migrate
+  #pytest ${out}
+  ${out}/manage.py import --all
+)
